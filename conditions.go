f3a010e5a979ccdafc423e39361b470d8e836c29
Nikita Panteleev <nikita.altipla@gmail.com>
2018-01-02 17:14:13 +0100

    Añade la condición de busqueda geografica objetos cercanos.
    Change-Id: I6d67314fa9e70d3902ec55346abfc54b65d2f7e5

---
+++ conditions.go
@@ -0,0 +1,45 @@
+package geo
+
+import (
+	"fmt"
+)
+
+// NearCondition stores the point and distance to filter around
+type NearCondition struct {
+	point Point
+	value float64
+}
+
+// Near filters by distance around a geographic point
+func Near(point Point, value float64) *NearCondition {
+	return &NearCondition{point, value}
+}
+
+// SQL returns the built SQL to apply the filter.
+func (cond *NearCondition) SQL() string {
+	const s string = `
+		6371 * 2 * ASIN(
+			SQRT(
+				POWER(
+					SIN(
+						RADIANS(%v - ABS(Y(position)))/2
+					),
+					2
+				)
+				+ COS(RADIANS(%v)) * COS(RADIANS(ABS(Y(position))))
+				* POWER(
+					SIN(
+						RADIANS(%v - X(position))/2
+					),
+					2
+				)
+			)
+		) < ?
+	`
+	return fmt.Sprintf(s, cond.point.Lng, cond.point.Lng, cond.point.Lat)
+}
+
+// Values returns the SQL placeholders to apply the filter
+func (cond *NearCondition) Values() []interface{} {
+	return []interface{}{cond.value}
+}
